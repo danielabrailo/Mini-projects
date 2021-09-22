c4748dc3bec03190d48a4c64561bea9b1de93ff7
Daniela Brailovsky <dbrailo@gmail.com>
2021-09-22 18:14:17 +0300

    Added calculator

---
+++ Calculator/app.js
@@ -0,0 +1,33 @@
+function add(a, b) {
+    return a + b
+}
+
+function substract(a, b) {
+    return a - b
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+function divide(a, b) {
+    return a / b
+}
+
+function operate(operator, a, b) {
+    switch(operator) {
+        case add:
+            return add(a, b)
+            break
+        case substract:
+            return substract(a, b)
+            break
+        case multiply:
+            return multiply(a, b)
+            break
+        case divide:
+            return divide(a, b)
+            break
+    }
+}
+
